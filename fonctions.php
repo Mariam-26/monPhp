dd6ba8124ef0c6f0456cf4bc9cbc715adb152c59
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-03-29 21:00:21 +0200

    monphp

---
+++ fonctions.php
@@ -0,0 +1,11 @@
+<?php 
+// LES FONCTIONS
+
+$mot = readline("Veuillez entrer un mot : ");
+$reverse = strtolower(strrev($mot));
+if(strtolower($mot) ===$reverse){
+  echo " Ce mot est un palyndrome";
+}else {
+  echo "Ce mot n'est pas un palyndrome";
+} 
+ ?>
