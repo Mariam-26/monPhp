965b384decf4f84e1d4cc1c87d425dcf81550a68
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-03-30 05:57:30 +0200

    mophp

---
+++ tableaux.php
@@ -0,0 +1,27 @@
+<?php
+// LES TABLEAUX
+
+
+// $eleve = ["Mariam", "Ouédraogo", [18, 13.65, 17.50, 16.50]];
+// echo $eleve[2][3];
+
+
+ $eleve = [
+   "nom" => "Ouédraogo",
+   "prenom" => "Mariam",
+   "notes" => [18, 13.65, 17.50, 16.50]
+ ];
+ echo $eleve["prenom"] . "\n";
+
+
+//  $eleve["prenom"] = "Olivia";
+//  $eleve["notes"][1] = 17;
+
+
+
+// PRINT_R => (représention graphique)
+// print_r($eleve["notes"]);
+
+
+
+// echo $eleve["prenom"] . " " . $eleve["nom"];
